1ad9a596367ea73667d8e90d4a21f635fc17930e
code_with_pado <padraigmullane@gmail.com>
2023-06-12 08:38:32 -0400

    feat: add function to decide the round outcome
    - Added a new function, `playRound`, to calculate the outcome of each
    round in the game.
    - The function has 'playerSelection' and 'computerSelection' as
    parameters to represent the choices made by the player and computer respectively.
    - The function compares the two parameters to determine the outcome of
    the round.
    - The function returns the outcome of the round (win, loss, or tie)
    to be used for scoring and displaying results.

--- validation.js
+++ validation.js
@@ -1,6 +1,6 @@
-//Create function getComputerChoice
-//Make randomNumber variable that randomly picks between 3 numbers using Math.Floor and Math.random
-//Assign each number to each of the 3 choices (rock,paper and scissors) 
+// Create function getComputerChoice
+// Make randomNumber variable that randomly picks between 3 numbers using Math.Floor and Math.random
+// Assign each number to each of the 3 choices (rock,paper and scissors) 
 function getComputerChoice(){
   let randomNumber= (Math.floor(Math.random))* 3;
   if(randomNumber=== 0){
@@ -11,5 +11,52 @@ function getComputerChoice(){
   }
   else{
     return ("scissors");
+  }
+}
+
+// Create variables playerScore and computerScore to keep track of score
+let playerScore = 0;
+let computerScore = 0;
+
+// Create playRound function with (playerSelection,computerSelection)
+// Compare the two parameters players
+// giDeclare the outcome of the round
+function playRound(playerSelection,computerSelection){
+  if (playerSelection === computerSelection){
+    return("It's a tie!");
+  }
+  else if(playerSelection === "rock"){
+        
+    if(computerSelection === "scissors"){
+      playerScore++;
+      return(`You chose ${playerSelection} and the computer chose${computerSelection}. You won! Rock beats scissors.`);
+    }
+    else{
+      computerScore++;
+      return(`You chose ${playerSelection} and the computer chose${computerSelection}. You lost! Paper beats rock.`);
+    }
+  }
+  else if(playerSelection === "paper"){
+        
+    if(computerSelection === "rock"){
+      playerScore++;
+      return(`You chose ${playerSelection} and the computer chose${computerSelection}. You won! Paper beats rock.`);
+    }
+
+    else{
+      computerScore++;
+      return(`You chose ${playerSelection} and the computer chose${computerSelection}. You lost! Scissors beats paper.`);
+    }
+  }
+  else if(playerSelection === "scissors"){
+        
+    if(computerSelection === "paper"){
+      playerScore++;
+      return(`You chose ${playerSelection} and the computer chose${computerSelection}. You won! Scissors beats paper.`);
+    }
+    else{
+      computerScore++;
+      return(`You chose ${playerSelection} and the computer chose${computerSelection}. You lost! Rock beats scissors.`);
+    }
   }
 }
